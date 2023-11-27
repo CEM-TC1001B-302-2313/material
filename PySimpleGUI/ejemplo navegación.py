a4c10812c96c52091377d96b5b66326d50f14ef4
aram-gonzalez-tec <aram.gonzalez@tec.mx>
2023-11-27 12:02:27 -0600

    Usuarios

--- PySimpleGUI/ejemplo navegación.py
+++ PySimpleGUI/ejemplo navegación.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import PySimpleGUI as sg
 
+sg.set_options(font="Arial 18")
+
 def crearVentanaLogin():
     col1 = sg.Col([
         [sg.Text("Email")],
@@ -92,7 +94,8 @@ while True:
         email = values["login input email"]
         contraseña = values["login input password"]
         df = pd.read_csv("usuarios.csv")
-        if len(df[(df["Email"] == email)&(df["Contraseña"] == contraseña)]) >= 1:            
+        if len(df[(df["Email"] == email)
+                  &(df["Contraseña"] == contraseña)]) >= 1:            
             window.close()
             ventanaLogin = None
             ventanaMenuPrincipal = crearVentanaMenuPrincipal()
@@ -106,3 +109,55 @@ while True:
         window.close()
         ventanaLogin = None
         ventanaRegistro = crearVentanaRegistro()
+    # Ventana Registro
+    # Registrar -> Menú Principal
+    elif window == ventanaRegistro and \
+         event == "registro button registro" and \
+         ventanaLogin is None:
+        email = values["registro input email"].strip()
+        password = values["registro input password"].strip()
+        repetir_password = values["registro input repetir password"].strip()
+        nombre = values["registro input nombre"].strip()
+        apellido_paterno = values["registro input apellido paterno"].strip()
+        apellido_materno = values["registro input apellido materno"].strip()
+        if values["registro radio notificaciones sí"]:
+            notificaciones = "sí"
+        else:
+            notificaciones = "no"
+        lista_errores = []
+        if "@" not in email:
+            lista_errores.append("El formato del correo es incorrecto.")
+        if not nombre:
+            lista_errores.append("Debes llenar el nombre.")
+        if not apellido_paterno:
+            lista_errores.append("Debes llenar el apellido paterno.")
+        if not apellido_materno:
+            lista_errores.append("Debes llenar el apellido materno.")
+        if not password:
+            lista_errores.append("Debes llenar la contraseña.")
+        if password != repetir_password:
+            lista_errores.append("Las contraseñas deben coincidir.")
+        # Revisamos que no haya errores en la lista
+        if not lista_errores:
+            nuevo_registro = pd.DataFrame([{
+                "Email": email,
+                "Contraseña": password,
+                "Nombre": nombre,
+                "Apellido paterno": apellido_paterno,
+                "Apellido materno": apellido_materno,
+                "Notificaciones": notificaciones
+                }])
+            df = pd.read_csv("usuarios.csv")
+            df = pd.concat([df, nuevo_registro], ignore_index=False)
+            df.to_csv("usuarios.csv", index=False)
+            sg.Popup("Registro exitoso", title="¡Éxito!")
+            window.close()
+            ventanaRegistro = None
+            ventanaLogin = crearVentanaLogin()
+        else:
+            sg.Popup("\n".join(lista_errores),
+                     title="Error en el registro.")
+        
+        
+        
+        
